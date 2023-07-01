b01ce7f4d38644c4d73d531f601be71b8a03bb71
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-07-01 23:19:24 +0500

    some work for assignment 03

--- lib/assignment-03/main.dart
+++ lib/assignment-03/main.dart
@@ -3,9 +3,26 @@ import 'dart:io';
 void main() {
   var methods = Assignments();
 
+
+  var evenNumber = methods.evenNumbers();
 }
 
 class Assignments {
   Assignments();
 
+  evenNumbers() {
+    List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    List<int>  evenNumbers = [];
+    for (var i = 0 ; i < numbers.length ; i++){
+      var number = numbers[i];
+      if (number % 2 == 0) {
+        evenNumbers.add(number);
+
+      }
+
+    }
+    print("Q1");
+    print(evenNumbers);
+
+  }
 }
