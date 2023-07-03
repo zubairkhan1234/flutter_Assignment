3c4ec1c78adba660d0250528a8aae11409a03fc1
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-07-03 18:46:43 +0500

    working for assignment 03

--- lib/assignment-03/main.dart
+++ lib/assignment-03/main.dart
@@ -3,8 +3,15 @@ import 'dart:io';
 void main() {
   var methods = Assignments();
 
+  //
+  // // Q 01
+  // var evenNumber = methods.evenNumbers();
+  //
+  // // Q 02
+  // var  fibonacciSequence = methods.fibonacciSequence();
 
-  var evenNumber = methods.evenNumbers();
+  // Q 03
+  var primeNumberChecker = methods.primeNumberChecker();
 }
 
 class Assignments {
@@ -25,4 +32,37 @@ class Assignments {
     print(evenNumbers);
 
   }
+
+  fibonacciSequence() {
+    print("Type any number which is greater than 15");
+    int n = int.parse(stdin.readLineSync()!);
+
+    int firstNum = 0;
+    int secondNum = 1;
+
+    print(firstNum); // Print the first number
+
+    for (int i = 0; i < n; i++) {
+      print(secondNum); // Print the current number
+      int nextNum = firstNum + secondNum;
+      firstNum = secondNum;
+      secondNum = nextNum;
+    }
+  }
+
+  primeNumberChecker(){
+
+
+    print("Type any Numbers");
+    int higherNumber =  int.parse(stdin.readLineSync()!) ;
+
+    for (int i = 2; i <= higherNumber / 2; i++) {
+      if (higherNumber % i == 0) {
+        return false; // Number is divisible by i, so it's not prime
+      }
+    }
+
+    print("$higherNumber is prime number");
+  }
+
 }
