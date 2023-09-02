e13fe97d3a591131a043e93beec93b91b6dec43a
zubairkhan1234 <zubairsaleem1210@gmail.com>
2023-09-02 21:00:17 +0500

    added custom fonts downloed from google

--- lib/figmaAssignment01/_app.dart
+++ lib/figmaAssignment01/_app.dart
@@ -12,8 +12,11 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "Manrope",
+      ),
       home: MyHomePage(),
     );
   }
--- lib/figmaAssignment01/screens/onboarding/_onBoarding.dart
+++ lib/figmaAssignment01/screens/onboarding/_onBoarding.dart
@@ -1,19 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutterassignment/figmaAssignment01/styles/_colors.dart';
 
-class Onboarding extends StatelessWidget {
-  const Onboarding({super.key});
+class Onboarding extends StatefulWidget {
+  const Onboarding({
+    Key? key,
+  }) : super(key: key);
 
   @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  int currentIndex = 0;
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(color: Color(GlobalColors.blueDark)),
-          child: Column(
-            children: [
-              Text("Your holiday shopping delivered to Screen one", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700, color:Color(GlobalColors.white), fontFamily: 'Raleway')),
-            ],
-          )),
-    );
+        backgroundColor: Color(GlobalColors.blackDark),
+        body: PageView(
+          children: [
+            Column(
+              children: [
+                Text("Your holiday shopping delivered to Screen one",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Color(GlobalColors.white),
+                        fontFamily: 'Manrope')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    currentIndex == 0 ? Icon(Icons.circle, size: 10, color: Color(GlobalColors.white)): Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+                    Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+                    Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white))
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text("Your holiday shopping delivered to Screen one",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Color(GlobalColors.white),
+                        fontFamily: 'Manrope')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+                    currentIndex == 1 ? Icon(Icons.circle, size: 10, color: Color(GlobalColors.white)): Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+                    Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white))
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                Text("Your holiday shopping delivered to Screen one",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w700,
+                        color: Color(GlobalColors.white),
+                        fontFamily: 'Raleway')),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+                    Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+                    currentIndex == 2 ? Icon(Icons.circle, size: 10, color: Color(GlobalColors.white)): Icon(Icons.circle_outlined, size: 10, color: Color(GlobalColors.white)),
+
+                  ],
+                )
+              ],
+            )
+          ],
+          onPageChanged: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ));
   }
 }
